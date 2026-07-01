338f7c9dd07ad7c91968fbb48c1aef2aea441746
Terts Diepraam <terts.diepraam@gmail.com>
2026-07-01 23:51:37 +0200

    use python script to generate content instead of JS

---
+++ main.py
@@ -0,0 +1,118 @@
+import requests
+from pprint import pprint
+from html.parser import HTMLParser
+from collections import defaultdict
+import json
+import re
+import csv
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+from datetime import datetime
+
+URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTYcli77i_Q8vR0zUyWOWO1J0so4Uq2w2tqXDIXDim6HMD9SDDUqVdtjRvywEPfDl2L2F5oWMVLA8ZV/pub?gid=451128911&single=true&output=tsv";
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.images = defaultdict(dict)
+        self.metas = {}
+        self.published = None
+
+    def handle_starttag(self, tag, attrs):
+        if tag != "meta":
+            return
+
+        attrs = dict(attrs)
+
+        try:
+            if attrs["property"] == "article:published_time":
+                self.published = attrs["content"]
+                return
+
+            if not attrs["property"].startswith("og:"):
+                return
+
+            if attrs["property"] == "og:image":
+                hid = attrs.get("data-hid") or "default"
+                self.images[hid]["src"] = attrs["content"]
+            elif attrs["property"] == "og:image:height":
+                hid = attrs.get("data-hid") or "default"
+                hid = hid.removesuffix("-height")
+                self.images[hid]["height"] = int(attrs["content"])
+            elif attrs["property"] == "og:image:width":
+                hid = attrs.get("data-hid") or "default"
+                hid = hid.removesuffix("-width")
+                self.images[hid]["width"] = int(attrs["content"])
+            else:
+                self.metas[attrs["property"]] = attrs["content"]
+        except:
+            pass
+
+def main():
+    driver = selenium.webdriver.Firefox()
+    response = requests.get(URL)
+
+    # For some reason, requests thinks its ISO-8859-1, but it is actually UTF-8
+    text = response.content.decode("UTF-8")
+
+    reader = csv.DictReader(text.split("\n"), dialect="excel-tab")
+
+    data = []
+
+    for line in reader:
+        new_data = {}
+
+        for key, value in line.items():
+            new_data[key.lower().strip()] = value
+    
+        try:
+            page_response = requests.get(new_data["url"])
+            text = page_response.text
+
+            parser = MyHTMLParser()
+            parser.feed(text)
+
+            # Remove some tracking info and stuff
+            if "og:url" in parser.metas:
+                new_data["url"] = parser.metas["og:url"]
+                
+            if parser.published:
+                new_data["published"] = parser.published
+            else:
+                # Bring out the big guns for Instagram
+                # We scrape all the time elements, which are present on both
+                # posts and comments. We assume that the time of the post is
+                # the oldest because all the comments must be made after the
+                # post.
+                driver.get(new_data["url"])
+                timestamps = driver.find_elements(By.XPATH, "//time")
+
+                timestamps = list(map(lambda t: t.get_attribute('datetime'), timestamps))
+
+                new_data["published"] = min(
+                    timestamps,
+                    key = lambda t: datetime.fromisoformat(t.replace('Z', '+00:00')),
+                )
+
+            new_data["metas"] = parser.metas
+            new_data["images"] = parser.images
+        except Exception as e:
+            print(e)
+            continue
+
+        data.append(new_data)
+
+    data.sort(reverse = True, key = lambda x: datetime.fromisoformat(x["published"]))
+
+    with open("articles.json", "w") as f:
+        json.dump(data, f, indent = 2)
+
+def has_attr(el, attr):
+    try:
+        el.get_attribute('datetime')
+        return True
+    except:
+        return False
+
+if __name__ == "__main__":
+    main()
--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-const URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTYcli77i_Q8vR0zUyWOWO1J0so4Uq2w2tqXDIXDim6HMD9SDDUqVdtjRvywEPfDl2L2F5oWMVLA8ZV/pub?gid=451128911&single=true&output=tsv";
+const URL = "./articles.json";
 
 async function reload() {
   const main = document.querySelector("main");
 
   const r = await fetch(URL);
-  const text = await r.text();
+  const articles = await r.json();
 
-  console.log(text);
-  
-  const lines = text.split("\n");
-  const header = lines[0].split("\t");
-  const data_lines = lines.slice(1);
+  for (article of articles) {
+    const img = document.createElement("img");
 
-  const data = [];
-
-  for (line of data_lines) {
-    const new_data = {}
-    const data_fields = line.split("\t");
-    for (let i = 0; i < header.length; i++) {
-      new_data[header[i].toLowerCase().trim()] = data_fields[i];
+    const imageLinks = Object.values(article["images"]);
+    if (imageLinks.length > 0) {
+      img.src = imageLinks[0]["src"];
     }
-    data.push(new_data)
-  }
+    img.classList.add("preview-image");
 
-  console.log(data);
-
-  for (datum of data) {
     const h2 = document.createElement("h2");
-    h2.innerText = datum.titel;
+    h2.innerText = article.titel;
 
     const a = document.createElement("a");
-    a.href = datum.url;
+    a.href = article.url;
     a.appendChild(h2);
 
-    main.appendChild(a);
+    const date = document.createElement("p");
+    date.classList.add("date")
+    if (article.published) {
+      const d = new Date(article.published);
+      date.innerText = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+    } else {
+      date.innerText = "unknown";
+    }
+
+    const rightDiv = document.createElement("div");
+    rightDiv.appendChild(a);
+    rightDiv.appendChild(date);
+    
+    const div = document.createElement("div");
+    div.classList.add("article");
+    div.appendChild(img);
+    div.appendChild(rightDiv);
+
+    main.appendChild(div);
   }
 
 }
